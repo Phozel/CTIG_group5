f24bd14e731e1084fc73e394198905380aa0bb8f
Phozel <thekoolstar@gmail.com>
2025-12-10 11:08:03 +0100

    Added audio manager, music manager, master volume control, and made mute button work

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -43,6 +43,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
         {   
             if (body.linearVelocityY <= 0)
                 body.AddForce(Vector2.up * jump_force, ForceMode2D.Impulse);
+            AudioManager.Instance.Play("Jump");
         }
 
     }
